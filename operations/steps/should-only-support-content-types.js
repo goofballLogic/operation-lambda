e78e8ef7775a78dcc6af9b39655aca402148356a
tinycode2 <tinycode2@gmail.com>
2016-11-02 22:56:18 +0000

    log incorrect content types

--- operations/steps/should-only-support-content-types.js
+++ operations/steps/should-only-support-content-types.js
@@ -3,6 +3,7 @@ module.exports = function shouldOnlySupportContentTypes( allowedContentTypes ) {
     const actualContentType = this.event.headers[ "Content-Type" ];
     if ( !~allowedContentTypes.indexOf( actualContentType ) ) {
         
+        console.warn( "Unsupported content type: " + actualContentType );
         const payload = this.payload( 415, "Unsupported content type: " + actualContentType, "text/plain" );
         return Promise.reject( payload );
 
